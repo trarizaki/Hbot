de416104784d715c534bbb88be59dff721f20b75
trarizaki <52135121+trarizaki@users.noreply.github.com>
2019-07-14 12:14:43 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -197,37 +197,14 @@ if (chan) {
 
 
 
-client.on('message', message => {
+client.on("message", message => {
+  if (message.content.includes("<@599535902708203550>")) {
+      message.channel.startTyping();
+      setTimeout(() => {
+        message.channel.stopTyping();
+      }, Math.random() * (1 - 3) + 1 * 1000);
    
-    let command = message.content.split(" ")[0];
-  command = command.slice(prefix.length);
-
-  let args = message.content.split(" ").slice(1);
-
-
-if(command == "draw") {
-    var Canvas = require('canvas')
-  , Image = new Canvas.Image
-  , canvas = new Canvas(450, 170)
-  , ctx = canvas.getContext('2d');
-  ctx.font = '30px Impact';
-  let args = message.content.split(" ").slice(1);
-  
-Image.src = canvas.toBuffer();
-
-    console.log(Image);
-ctx.drawImage(Image, 0, 0, Image.width / 470, Image.height / 170);
-ctx.fillText(args.join("  "),110, 70);
-
-
-ctx.beginPath();
-ctx.lineTo(50, 102);
-ctx.stroke();
-
-message.channel.sendFile(canvas.toBuffer());
-}
-}).on('ready', () => {
-
+  }
 });
 
 
